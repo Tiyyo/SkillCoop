32810624a046a1d42fe179cf353531c72a09583b
Steeve Matou <111092370+Tiyyo@users.noreply.github.com>
2023-09-24 13:33:17 +0200

    fix: profile infos and profile skill are now independant

--- apps/api/app/models/profile.ts
+++ apps/api/app/models/profile.ts
@@ -43,15 +43,17 @@ export class Profile extends Core {
       throw new DatabaseError(error)
     }
   }
-  async findOne(id: number) {
+  async findOne( id : number) {
+    const infos = await this.client
+      .selectFom('profile')
+      .select(['profile.user_id', 'profile.avatar_url', 'profile.username', 'profile.date_of_birth', 'profile.id as profile_id'])
+      .where('profile_id', "=", id)
+      .execute()
 
-    try {
-      const result = await this.client
-        .selectFrom('profile')
-        .select(['profile.user_id', 'profile.avatar_url', 'profile.username', 'profile.date_of_birth', 'profile.id as profile_id'])
-        .innerJoin('skill_foot', 'profile.id', 'skill_foot.reviewee_id')
-        //@ts-ignore
-        .select(({ fn }) => [
+  const skills = await this.client
+      .selectFrom('profile')
+      .innerJoin('skill_foot', 'profile.id', 'skill_foot.reviewee_id')
+      .select(({ fn }) => [
           'profile.id',
           fn.count('profile.id').as('nb_review'),
           fn.avg('skill_foot.pace').as('avg_pace'),
@@ -64,21 +66,56 @@ export class Profile extends Core {
         .groupBy('profile.id')
         .execute()
 
-
-      result[0].gb_rating = computeGbRating({
-        avg_pace: result[0].avg_pace,
-        avg_shooting: result[0].avg_shooting,
-        avg_passing: result[0].avg_passing,
-        avg_dribbling: result[0].avg_dribbling,
-        avg_defending: result[0].avg_defending,
+    skills[0].gb_rating = computeGbRating({
+        avg_pace: skills[0].avg_pace,
+        avg_shooting: skills[0].avg_shooting,
+        avg_passing: skills[0].avg_passing,
+        avg_dribbling: skills[0].avg_dribbling,
+        avg_defending: skills[0].avg_defending,
       })
+    
+      const profile = {
+        ...infos[0], skills : skills[0] 
+      }
 
-      return result[0]
-    } catch (error) {
-      throw new DatabaseError(error)
-    }
-
+    return profile
   }
+  // async findOne(id: number) {
+
+  //   try {
+  //     const result = await this.client
+  //       .selectFrom('profile')
+  //       .select(['profile.user_id', 'profile.avatar_url', 'profile.username', 'profile.date_of_birth', 'profile.id as profile_id'])
+  //       .innerJoin('skill_foot', 'profile.id', 'skill_foot.reviewee_id')
+  //       //@ts-ignore
+  //       .select(({ fn }) => [
+  //         'profile.id',
+  //         fn.count('profile.id').as('nb_review'),
+  //         fn.avg('skill_foot.pace').as('avg_pace'),
+  //         fn.avg('skill_foot.shooting').as('avg_shooting'),
+  //         fn.avg('skill_foot.passing').as('avg_passing'),
+  //         fn.avg('skill_foot.dribbling').as('avg_dribbling'),
+  //         fn.avg('skill_foot.defending').as('avg_defending'),
+  //       ])
+  //       .where('profile_id', "=", id)
+  //       .groupBy('profile.id')
+  //       .execute()
+
+
+  //     result[0].gb_rating = computeGbRating({
+  //       avg_pace: result[0].avg_pace,
+  //       avg_shooting: result[0].avg_shooting,
+  //       avg_passing: result[0].avg_passing,
+  //       avg_dribbling: result[0].avg_dribbling,
+  //       avg_defending: result[0].avg_defending,
+  //     })
+
+  //     return result[0]
+  //   } catch (error) {
+  //     throw new DatabaseError(error)
+  //   }
+
+  // }
   // TODO define a type for data
   async create(data) {
 
@@ -104,14 +141,41 @@ export class Profile extends Core {
       .executeTakeFirst()
     return result
   }
-  async findByUserId(id: number) {
-    try {
-      const result = await this.client
-        .selectFrom('profile')
-        .select(['profile.user_id', 'profile.avatar_url', 'profile.username', 'profile.date_of_birth', 'profile.id as profile_id'])
-        .innerJoin('skill_foot', 'profile.id', 'skill_foot.reviewee_id')
-        //@ts-ignore
-        .select(({ fn }) => [
+  // async findByUserId(id: number) {
+  //   try {
+  //     const result = await this.client
+  //       .selectFrom('profile')
+  //       .select(['profile.user_id', 'profile.avatar_url', 'profile.username', 'profile.date_of_birth', 'profile.id as profile_id'])
+  //       .innerJoin('skill_foot', 'profile.id', 'skill_foot.reviewee_id')
+  //       //@ts-ignore
+  //       .select(({ fn }) => [
+  //         'profile.id',
+  //         fn.count('profile.id').as('nb_review'),
+  //         fn.avg('skill_foot.pace').as('avg_pace'),
+  //         fn.avg('skill_foot.shooting').as('avg_shooting'),
+  //         fn.avg('skill_foot.passing').as('avg_passing'),
+  //         fn.avg('skill_foot.dribbling').as('avg_dribbling'),
+  //         fn.avg('skill_foot.defending').as('avg_defending'),
+  //       ])
+  //       .where('profile.user_id', "=", id)
+  //       .groupBy('profile.id')
+  //       .execute()
+  //     return result[0]
+  //   } catch (error) {
+  //     throw new DatabaseError(error)
+  //   }
+  // }
+    async findByUserId( id : number) {
+    const infos = await this.client
+      .selectFom('profile')
+      .select(['profile.user_id', 'profile.avatar_url', 'profile.username', 'profile.date_of_birth', 'profile.id as profile_id'])
+      .where('profile.user_id', "=", id)
+      .execute()
+
+  const skills = await this.client
+      .selectFrom('profile')
+      .innerJoin('skill_foot', 'profile.id', 'skill_foot.reviewee_id')
+      .select(({ fn }) => [
           'profile.id',
           fn.count('profile.id').as('nb_review'),
           fn.avg('skill_foot.pace').as('avg_pace'),
@@ -123,9 +187,19 @@ export class Profile extends Core {
         .where('profile.user_id', "=", id)
         .groupBy('profile.id')
         .execute()
-      return result[0]
-    } catch (error) {
-      throw new DatabaseError(error)
-    }
+
+    skills[0].gb_rating = computeGbRating({
+        avg_pace: skills[0].avg_pace,
+        avg_shooting: skills[0].avg_shooting,
+        avg_passing: skills[0].avg_passing,
+        avg_dribbling: skills[0].avg_dribbling,
+        avg_defending: skills[0].avg_defending,
+      })
+    
+      const profile = {
+        ...infos[0], skills : skills[0] 
+      }
+
+    return profile
   }
 }

5f1c6746eaf023443ca32f4b16b2663f930e9615
Steeve Matou <steeve.matou@gmail.com>
2023-11-14 21:29:28 +0100

    feat: event status badge is display in red when event is cancelled

--- apps/client/src/component/badge/index.tsx
+++ apps/client/src/component/badge/index.tsx
@@ -1,10 +1,19 @@
+import { cn } from '../../lib/utils';
+import { eventStatus } from '../../types';
+
 interface BadgeProps {
   content: string;
 }
 
 function Badge({ content }: BadgeProps) {
   return (
-    <div className="uppercase text-xxs font-semibold border border-primary-600 py-0.5 px-1.5 bg-primary-300 rounded-lg">
+    <div
+      className={cn(
+        `uppercase text-xxs font-semibold border
+     border-primary-600 py-0.5 px-1.5 bg-primary-300 rounded-lg`,
+        content === eventStatus.cancelled && 'bg-red-100 border-red-300',
+      )}
+    >
       {content}
     </div>
   );

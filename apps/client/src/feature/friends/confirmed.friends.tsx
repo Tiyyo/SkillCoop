86b34332a85a21b7a3a0a065544e7ddf1d64c2ab
Steeve Matou <steeve.matou@gmail.com>
2023-11-16 11:04:50 +0100

    refactor: confirmed friends page

--- apps/client/src/feature/friends/confirmed.friends.tsx
+++ apps/client/src/feature/friends/confirmed.friends.tsx
@@ -1,45 +1,24 @@
-import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
-import { getFriendsFn } from '../../api/api.fn';
 import Friendlist from './list.friends';
-import { useFriends } from '../../store/friend.store';
-import { useEffect } from 'react';
 import TitleH2 from '../../component/title-h2';
 import { useApp } from '../../store/app.store';
+import { useConfirmedfriends } from '../../hooks/useConfirmedFriends';
 
 function ConfirmedFriends() {
   // TODO implement infinite scroll
   const { userProfile } = useApp();
-  const { addConfirmedFriends, confirmedFriends } = useFriends();
   const profileId = userProfile?.profile_id;
-  const {
-    data: friends,
-    isLoading,
-    isFetching,
-    isError,
-  } = useQuery(
-    ['getFriends'],
-    () => {
-      if (!profileId) return;
-      return getFriendsFn(profileId);
-    },
-    {
-      enabled: true,
-    },
-  );
-  const loading = isLoading || isFetching;
-
-  useEffect(() => {
-    if (!friends) return;
-    addConfirmedFriends(friends);
-  }, [friends]);
+  const { confirmedFriends, loading, isError } = useConfirmedfriends({
+    profileId,
+  });
 
   return (
     <>
       <Link
         to="pending-request"
-        className="text-end px-3 text-xs py-2 text-primary-900 underline-offset-4 underline transition-all duration-300 hover:text-primary-1100 cursor-pointer
-      "
+        className="text-end px-3 text-xs py-2 text-primary-900 
+          underline-offset-4 underline transition-all duration-300 
+          hover:text-primary-1100 cursor-pointer"
       >
         See pending request
       </Link>
---
+++ apps/client/src/hooks/useConfirmedFriends.ts
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { useFriends } from '../store/friend.store';
+import { useGetConfirmedFriends } from './useFriends';
+
+export function useConfirmedfriends({ profileId }: { profileId?: number }) {
+  const { addConfirmedFriends, confirmedFriends } = useFriends();
+  const { data, isLoading, isFetching, isError } = useGetConfirmedFriends({
+    profileId,
+  });
+
+  const loading = isLoading || isFetching;
+
+  useEffect(() => {
+    if (!data) return;
+    addConfirmedFriends(data);
+  }, [data]);
+
+  return { confirmedFriends, loading, isError };
+}

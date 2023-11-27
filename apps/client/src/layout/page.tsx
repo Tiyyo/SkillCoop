4daa1bbf887bdf6a8b761323fa1202553d38cec5
Steeve Matou <steeve.matou@gmail.com>
2023-11-27 12:10:42 +0100

    feat: nav button link desktop sidemenu

--- apps/client/src/layout/page.tsx
+++ apps/client/src/layout/page.tsx
@@ -1,7 +1,12 @@
+import SideMenuDesktop from '../test-desktop/side-menu';
+
 function Page({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen bg-base relative pb-8">
-      {children}
+    <div className="lg:flex lg: w-screen lg:">
+      <SideMenuDesktop />
+      <div className="flex flex-col min-h-screen bg-base relative pb-8 w-full">
+        {children}
+      </div>
     </div>
   );
 }
---
+++ apps/client/src/test-desktop/side-menu.tsx
@@ -0,0 +1,41 @@
+import { Calendar } from 'lucide-react';
+import { Link, NavLink } from 'react-router-dom';
+import NavButtonDesktop from './nav-button-desktop';
+
+function SideMenuDesktop() {
+  return (
+    <aside
+      className="hidden lg:flex w-[20%] lg:flex-col min-w-[240px]
+         bg-dark text-sm text-grey-regular font-medium"
+    >
+      <div className="flex justify-center h-fit w-full pt-6 pb-12">
+        <div className="rounded-full h-10 aspect-square bg-primary-700"></div>
+      </div>
+      <ul className="w-full">
+        <NavButtonDesktop path="/" content="Home">
+          <Calendar size={14} />
+        </NavButtonDesktop>
+        <NavButtonDesktop path="/events/incoming" content="Upcoming Events">
+          <Calendar size={14} />
+        </NavButtonDesktop>
+        <NavButtonDesktop path="/events/past" content="Past Events">
+          <Calendar size={14} />
+        </NavButtonDesktop>
+        <NavButtonDesktop path="/my-event" content="My Events">
+          <Calendar size={14} />
+        </NavButtonDesktop>
+        <NavButtonDesktop path="/contact" content="Friends">
+          <Calendar size={14} />
+        </NavButtonDesktop>
+        {/* <NavButtonDesktop path="/my-event" content="Chat">
+          <Calendar size={14} />
+        </NavButtonDesktop> */}
+        <NavButtonDesktop path="/user/profile" content="Profile">
+          <Calendar size={14} />
+        </NavButtonDesktop>
+      </ul>
+    </aside>
+  );
+}
+
+export default SideMenuDesktop;

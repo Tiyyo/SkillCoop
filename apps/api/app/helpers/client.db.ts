e33f4866703dc7983f8bac018afd9f006b3e60ab
Steeve Matou <steeve.matou@gmail.com>
2024-01-29 23:06:36 +0100

    fix : link to prod db which was broke with node update to lts

--- apps/api/app/helpers/client.db.ts
+++ apps/api/app/helpers/client.db.ts
@@ -2,8 +2,13 @@ import { DB as DatabaseType } from '../@types/database.js';
 import SQLite from 'better-sqlite3';
 import { Kysely, SqliteDialect } from 'kysely';
 
+const sqliteDBPath =
+  process.env.NODE_ENV === 'production'
+    ? './apps/api/prisma/sqlite.db'
+    : './prisma/sqlite.db';
+
 const dialect = new SqliteDialect({
-  database: new SQLite('./prisma/sqlite.db'),
+  database: new SQLite(sqliteDBPath),
 });
 
 export const db = new Kysely<DatabaseType>({ dialect });

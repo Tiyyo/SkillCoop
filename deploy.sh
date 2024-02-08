6aa54074345caf6637d4597b963dc4cc48d77132
Steeve Matou <steeve.matou@gmail.com>
2024-02-08 17:22:13 +0100

    chore: update GHA

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+echo "Stop all apps"
+sudo pm2 kill
+
+echo "Go to the project path"
+cd /Skillcoop
+
+echo "Pulling latest code"
+git pull origin main
+
+echo "Install dependencies"
+sudo pnpm Install
+
+echo "Build the project"
+sudo pnpm schema build
+sudo pnpm date build
+sudo pnpm types build
+sudo pnpm api build 
+sudo pnpm chat build
+
+echo "Migration the database"
+sudo pnpm api db:migrate:deploy
+
+echo "Start all apps"
+sudo pm2 start skillcoop-pm2.json

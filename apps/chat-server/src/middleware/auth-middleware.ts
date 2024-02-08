ababfb059adb331ad601ae497e1bb3026c5a4d0c
Steeve Matou <steeve.matou@gmail.com>
2024-02-08 21:57:58 +0100

    fix: add more logs to chat server for prod

--- apps/chat-server/src/middleware/auth-middleware.ts
+++ apps/chat-server/src/middleware/auth-middleware.ts
@@ -10,13 +10,20 @@ export class AuthMiddleware implements NestMiddleware {
   }
   async use(req: Request, _res: Response, next: NextFunction) {
     const token = req.cookies.refreshToken
+    console.log('token', token)
+    Logger.debug('token', token)
+    console.log('req.cookies', req.cookies)
+    Logger.debug('req.cookies', req.cookies)
     if (!token) {
+      console.log('No token found')
+      Logger.debug('No token found')
       throw new UnauthorizedException()
     }
     try {
       await this.jwtService.verifyAsync(token, { secret: process.env.JWT_REFRESH_TOKEN_KEY })
     } catch (error) {
       Logger.error(`Error validating token :`, error)
+      console.log('Error validating token :', error)
       throw new UnauthorizedException()
     }
     next()

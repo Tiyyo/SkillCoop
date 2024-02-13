acb67fd17917f0ae764542e8f3db62af708b2bb6
Steeve Matou <steeve.matou@gmail.com>
2024-02-13 12:45:22 +0100

    fix: adjust path to csv file in production env

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -32,9 +32,7 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const pathToFile =
-  process.env.NODE_ENV === 'production'
-    ? '../dist/prisma/data.csv'
-    : 'data.csv';
+  process.env.NODE_ENV === 'production' ? '../../primsa/data.csv' : 'data.csv';
 
 async function getPlaygroundJson() {
   const filePath = path.join(__dirname, pathToFile);

3dfe8c546ae08dde18b97f6cba8cdba37e491e25
Steeve Matou <steeve.matou@gmail.com>
2024-03-01 00:47:35 +0100

    fix: remove helmet

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,6 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import helmet from 'helmet';
 import { WinstonModule } from 'nest-winston';
 import { winstonLogger } from './infrastructure/logger/winston.logger';
 import { AllExceptionFilter } from './infrastructure/exceptions/exception.filter';
@@ -13,15 +12,15 @@ async function bootstrap() {
       instance: winstonLogger,
     }),
   });
-  app.use(helmet());
-  app.use(cookieParser());
-  app.setGlobalPrefix('api', {
-    exclude: [{ path: 'auth/google/callback', method: RequestMethod.ALL }],
-  });
   app.enableCors({
     origin: ['https://skillcoop.fr'],
     allowedHeaders: ['content-type', 'Authorization'],
     credentials: true,
+  });
+  // app.use(helmet());
+  app.use(cookieParser());
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'auth/google/callback', method: RequestMethod.ALL }],
   });
 
   app.useGlobalPipes(
